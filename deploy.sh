53c27c2c3e38a54ae8076667ea178202a83a80e7
Victor Moiseev <vim@psu.ru>
2021-07-05 16:47:48 +0500

    Успешно создается. Остался баг с ансибл линт. Тестировать.

--- deploy.sh
+++ deploy.sh
@@ -10,9 +10,9 @@ INSTANCE_SSH_KEY="${WD}/SGK1.pem"
 echo "======== CREATING VPC ======"
 
 cd ${TF_DIR}
-#terraform init   || exit 1
-#terraform plan   || exit 1
-#terraform apply
+terraform init   || exit 1
+terraform plan   || exit 1
+terraform apply
 
 WEB_ID=`aws ec2 describe-instances | jq '."Reservations"[] | ."Instances"[] | ."InstanceId"' | tr -d '"'`
 WEB_IP=`aws ec2 describe-instances | jq '."Reservations"[] | ."Instances"[] | ."PublicIpAddress"' | tr -d '"' `
@@ -20,7 +20,7 @@ WEB_IP=`echo ${WEB_IP} | grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9
 
 echo "======== CHECK INSTANCE ======"
 
-ping ${WEB_IP} -c 10
+#ping ${WEB_IP} -c 10
 
 echo "======== CONFIGURING INSTANCE ======"
 
@@ -28,4 +28,9 @@ cd ${ANS_DIR}
 echo "[aws_ec2_instance_ip]" > inventory.yml
 echo -e ${WEB_IP} >> inventory.yml
 
-ansible-playbook playbook.yml -i inventory.yml --key-file "${INSTANCE_SSH_KEY}"
+ansible-playbook playbook.yml -i inventory.yml --key-file "${INSTANCE_SSH_KEY}" --diff -v
+
+echo "======== CHECK SERVICE ======"
+ping ${WEB_IP} -c 10
+curl http://${WEB_IP}/
+
--- terraform-ec2/modules/vpc/vpc.tf
+++ terraform-ec2/modules/vpc/vpc.tf
@@ -63,6 +63,13 @@ resource "aws_security_group" "public_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port = -1
+    to_port = -1
+    protocol = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port = 0
     protocol = "-1"
